726af5378914b359e1037081e3116024fe0d4860
Kiran Anto <76390123+kiran-Anto@users.noreply.github.com>
2022-03-18 19:31:19 +0530

    Wordpress assignment
    added edited files

---
+++ header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+?>
+<!DOCTYPE html>
+<html class="<?php echo esc_attr( oceanwp_html_classes() ); ?>" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="styles.css">
+
+	<?php wp_head(); ?>
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+	<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	
+</head>
+
+<body <?php body_class(); ?> <?php oceanwp_schema_markup( 'html' ); ?>>
+
+	<?php wp_body_open(); ?>
+
+	<?php do_action( 'ocean_before_outer_wrap' ); ?>
+
+	<div id="outer-wrap" class="site clr">
+
+		<a class="skip-link screen-reader-text" href="#main"><?php oceanwp_theme_strings( 'owp-string-header-skip-link', 'oceanwp' ); ?></a>
+
+		<?php do_action( 'ocean_before_wrap' ); ?>
+
+		<div id="wrap" class="clr">
+
+			<?php do_action( 'ocean_top_bar' ); ?>
+
+			<?php do_action( 'ocean_header' ); ?>
+
+			<?php do_action( 'ocean_before_main' ); ?>
+
+			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
+
+				<?php do_action( 'ocean_page_header' ); ?>
+				
+				<nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <h5><a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a></h5>
+      </li>
+      <li class="nav-item">
+		  <h5><a class="nav-link" href="blog">Blogs</a></h5>
+      </li>
+      <li class="nav-item">
+        <h5><a class="nav-link" href="contact">Contact us</a></h5>
+      </li>
+    </ul>
+  </div>
+</nav>
+				
+<div>
+	<img src="<?php echo get_field('bgimage'); ?>">
+		 <h2><?php echo get_field('subtitle'); ?></h2><br>
+				</div>
+				<div class="vid">
+					<?php echo get_field('embedvid'); ?>
+				</div>
+				<div class="blogw">
+					<img src="<?php echo get_field('mart'); ?>" style="width:500px">
+				</div>
+	<div id="sub">
+				<h5><?php echo get_field('subtext'); ?></h5>
+</div>				
+
+				

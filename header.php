6ea2b32accd24669f7167407ccc8fd2c2fc18c93
Kiran Anto <76390123+kiran-Anto@users.noreply.github.com>
2022-03-22 16:56:50 +0530

    Add files via upload
    updated nav bar

--- header.php
+++ header.php
@@ -11,14 +11,14 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="styles.css">
+	
 
 	<?php wp_head(); ?>
 	<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	
-	<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	
 </head>
 
@@ -45,26 +45,28 @@
 			<main id="main" class="site-main clr"<?php oceanwp_schema_markup( 'main' ); ?> role="main">
 
 				<?php do_action( 'ocean_page_header' ); ?>
-				
-				<nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <h5><a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a></h5>
-      </li>
-      <li class="nav-item">
-		  <h5><a class="nav-link" href="blog">Blogs</a></h5>
-      </li>
-      <li class="nav-item">
-        <h5><a class="nav-link" href="contact">Contact us</a></h5>
-      </li>
-    </ul>
-  </div>
+			
+				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <h5><a class="nav-link" href="http://127.0.0.1:81/wordpress/">Home
+              <span class="sr-only">(current)</span>
+				</a></h5>
+          </li>
+          <li class="nav-item">
+			  <h5><a class="nav-link" href="http://127.0.0.1:81/wordpress/blog">Blogs</a></h5>
+          </li>
+          <li class="nav-item">
+			  <h5><a class="nav-link" href="http://127.0.0.1:81/wordpress/contact">Contact us</a></h5>
+          </li>
+		  </ul>
+</div>
 </nav>
+					
 				
 <div>
 	<img src="<?php echo get_field('bgimage'); ?>"<<br>
@@ -77,7 +79,7 @@
 					<img src="<?php echo get_field('mart'); ?>" style="width:500px">
 				</div>
 	<div id="sub"
-				<<h5><?php echo get_field('subtext'); ?></h5>
+				<h5><?php echo get_field('subtext'); ?></h5>
 </div>				
 
 				
